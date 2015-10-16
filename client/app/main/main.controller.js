1502d5d293b86a95b46dd23617656d91a4081307
Michael Bray <bray_michael@bah.com>
2015-10-16 13:55:08 -0400

    Added spider graph

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,11 +4,6 @@ angular.module('ghgVisualizerApp')
   .controller('MainCtrl', function ($scope, $http) {
       $scope.selectedAgency = null;
 
-      $scope.gasMult = 8.85928732;
-      $scope.dieselMult = 7.40266869565217;
-      $scope.hybridMult = 1.335297848;
-      $scope.electricMult = 0;
-
       function loadAgencies() {
         $.ajax({
           url: "/getAgencies",
@@ -28,25 +23,14 @@ angular.module('ghgVisualizerApp')
         });
 
         function agencySelected(agency){
-          // GET VEHICLE STATS
-
           $.ajax({
             url: "/getVehicleStats/" + agency,
             context: document.body
           }).done(function(data) {
             console.log(data);
             renderPieChart('vehiclePieChart', data.agencyName, data.vehiclePairs, data.efficientCarPercentage);
-          });
-
-          // GET
-
-          $.ajax({
-            url: "/getVehicleEmissions/" + agency,
-            context: document.body
-          }).done(function(data) {
-            console.log(data);
-
-
+            renderBarChart('emissionsBarChart',agency,data);
+            renderSpiderChart('spiderChart',agency,data);
           });
 
           $('#initialGraphs').show();
@@ -59,8 +43,11 @@ angular.module('ghgVisualizerApp')
         console.log(id);
         $('#' + id).highcharts({
           chart: {
-            type: 'pie', options3d: {
-              enabled: true, alpha: 45, beta: 0
+            type: 'pie',
+            options3d: {
+              enabled: true,
+              alpha: 45,
+              beta: 0
             }
           }, credits: {
             enabled: false
@@ -72,13 +59,143 @@ angular.module('ghgVisualizerApp')
             pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
           }, plotOptions: {
             pie: {
-              allowPointSelect: true, cursor: 'pointer', depth: 35, dataLabels: {
-                enabled: true, format: '{point.name}'
+              allowPointSelect: true,
+              cursor: 'pointer',
+              depth: 35,
+              dataLabels: {
+                enabled: true,
+                format: '{point.name}'
               }
             }
           }, series: [{
             type: 'pie', name: 'Number of Vehicles', data: data
           }]
+        });
+      }
+
+      function renderBarChart(id, agency, data){
+        $scope.spiderSummary = "Test";
+
+        $('#'+id).highcharts({
+          chart: {
+            type: 'column'
+          }, credits: {
+            enabled: false
+          },
+          exporting: {
+            enabled: false
+          },
+          title: {
+            text: '2014 GHG Emissions by Vehicle Type'
+          },
+          xAxis: {
+            categories: data.vehicleTypes
+          },
+          yAxis: {
+            min: 0,
+            title: {
+              text: 'Total GHG Emissions'
+            },
+            stackLabels: {
+              enabled: true,
+              style: {
+                fontWeight: 'bold',
+                color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+              }
+            }
+          },
+          legend: {
+            align: 'right',
+            x: -30,
+            verticalAlign: 'top',
+            y: 25,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            shadow: false
+          },
+          tooltip: {
+            formatter: function () {
+              return '<b>' + this.x + '</b><br/>' +
+                  this.series.name + ': ' + this.y + '<br/>' +
+                  'Total: ' + this.point.stackTotal;
+            }
+          },
+          plotOptions: {
+            column: {
+              stacking: 'normal',
+              dataLabels: {
+                enabled: false,
+                color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                style: {
+                  textShadow: '0 0 3px black'
+                }
+              }
+            }
+          },
+          series: [{
+            name: data.agencyName,
+            data: data.vehicleEmissions
+          }]
+        });
+      }
+
+      function renderSpiderChart(id, agency, data){
+        $("#"+id).highcharts({
+          credits: {
+            enabled: false
+          },
+          exporting: {
+            enabled: false
+          },
+          chart: {
+            polar: true,
+            type: 'line'
+          },
+
+          title: {
+            text: "",
+            x: 0
+          },
+
+          pane: {
+            size: '100%'
+          },
+
+          xAxis: {
+            categories: data.vehicleTypes,
+            tickmarkPlacement: 'on',
+            lineWidth: 0
+          },
+
+          yAxis: {
+            gridLineInterpolation: 'polygon',
+            lineWidth: 0,
+            min: 0
+          },
+
+          tooltip: {
+            shared: true,
+            pointFormat: '<span style="color:{series.color}">{series.name}: <b>{point.y:,.0f}%</b><br/>'
+          },
+
+          legend: {
+            align: 'center',
+            verticalAlign: 'bottom',
+            y: 0,
+            layout: 'vertical'
+          },
+
+            series: [{
+              type: 'area',
+              name: 'Percent of GHG Emissions',
+              data: data.polarGHG
+            },{
+              type: 'line',
+              name: 'Percent of Fleet',
+              data: data.polarCars
+            }]
         });
       }
   });
